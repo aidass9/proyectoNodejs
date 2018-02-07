a37a5d8ef8fb10a5b8b215fdf64b70680f0fd30e
Aida <aida9.soriano@gmail.com>
2018-02-07 17:59:04 +0100

    acabat

--- crearBaseDatos.sql
+++ crearBaseDatos.sql
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS Carreras(
 CREATE TABLE IF NOT EXISTS Participantes(
  idParticipante int NOT NULL AUTO_INCREMENT,
  numCarrera int NOT NULL,
- dorsal INT NOT NULL,
+ dorsal INT NOT NULL UNIQUE,
  nombre VARCHAR(50) NOT NULL,
  apellidos varchar(50) NOT NULL,
  posicionGeneral int NOT NULL,
  DNI varchar(50) NOT NULL,
  tiempoOficial varchar(50),
  PRIMARY KEY(idParticipante),
- CONSTRAINT `FK_participantes_carreras` FOREIGN KEY (`idParticipante`) REFERENCES `carreras` (`numCarrera`),
+ CONSTRAINT `FK_participantes_carreras` FOREIGN KEY (`numCarrera`) REFERENCES `carreras` (`numCarrera`),
  KEY (dorsal)
 );
 
--- routes/participantes.js
+++ routes/participantes.js
@@ -20,16 +20,30 @@ router.get('/borrar/:id', function (req, res, next) {
     });
 });
 
-router.get('/editar/:id', function (req, res, next) {
+router.get('/editar/:idParticipante', function (req, res, next) {
+    bd.query("SELECT * FROM Participantes WHERE ?", req.params, function (error, participante) {
+        if (error) {
+            cargarVistaInicio(res, error.message, false);
+        }
+        else if (participante.length) {
+            console.log(participante[0]);
+            res.render('participantes/editar', { p: participante[0] });
+        }
+        else {
+            cargarVistaInicio(res, "No existe el participante", false);
+
+        }
+    });
+
 
 });
 
 router.get('/crear', function (req, res, next) {
-
+    res.render('participantes/crear');
 });
 
-router.get('/detalle/:id', function (req, res, next) {
-    bd.query("SELECT * FROM Participantes WHERE idParticipantes = ?", req.params, function (error, participante) {
+router.get('/detalle/:idParticipante', function (req, res, next) {
+    bd.query("SELECT * FROM Participantes WHERE ?", req.params, function (error, participante) {
         if (error) {
             cargarVistaInicio(res, error.message, false);
         }
@@ -42,11 +56,40 @@ router.get('/detalle/:id', function (req, res, next) {
     });
 });
 
-router.post('/editar', function (req, res, next) {
+router.post('/editar/:idParticipante', function (req, res, next) {
 
+    bd.query('UPDATE Participantes SET ? WHERE ?', [req.body, { idParticipante: req.params.idParticipante }], function (error, resultado) {
+        if (error) {
+            bd.query("SELECT * FROM Participantes WHERE ?", req.params, function (error2, participante) {
+                if (error2) {
+                    cargarVistaInicio(res, error2.message, false);
+                }
+                else if (participante.length) {
+                    console.log(participante[0]);
+                    res.render('participantes/editar', { p: participante[0], mensaje: error.message });
+                }
+                else {
+                    cargarVistaInicio(res, "No existe el participante", false);
+        
+                }
+            });
+        }
+        else {
+            cargarVistaInicio(res, 'Participante editado con éxito', true);
+        }
+    });
 });
 
 router.post('/crear', function (req, res, next) {
+
+    bd.query("INSERT INTO Participantes SET ?", req.body, function (error, resultado) {
+        if (error) {
+            res.render('participantes/crear', { mensaje: error.message });
+        }
+        else {
+            cargarVistaInicio(res, 'Participante creado con éxito', true);
+        }
+    });
 
 });
 
